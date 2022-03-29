affd8eeb861a556864fa00973141d2640727e13c
Bonus TM <bonus.tm@gmail.com>
2022-03-30 00:35:00 +0300

    Поправил пути импортов в main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import './css/main.css'
-import './css/ck.css'
+import App from '@/App.vue'
+import '@/css/main.css'
+import '@/css/ck.css'
 
 createApp(App).mount('#app')
